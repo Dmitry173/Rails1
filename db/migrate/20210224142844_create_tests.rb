e5ef384de008df1dbfdcb5b9e27aa1c3fc7cdbac
Dmitry Shevchuk <dmitiry.shevchuk@mail.ru>
2021-02-25 17:02:54 +0300

    Changes to be committed: 	modified:   db/migrate/20210224142844_create_tests.rb 	modified:   db/migrate/20210224142850_create_categories.rb 	modified:   db/migrate/20210224142855_create_questions.rb 	modified:   db/migrate/20210224142909_create_answers.rb 	modified:   db/migrate/20210224142913_create_users.rb 	deleted:    db/migrate/20210224143056_add_reference_index_to_all_tables.rb 	deleted:    db/migrate/20210224143102_add_tests_level_default.rb 	deleted:    db/migrate/20210224143106_add_to_all_tables_null_constraints.rb

--- db/migrate/20210224142844_create_tests.rb
+++ db/migrate/20210224142844_create_tests.rb
@@ -1,8 +1,10 @@
 class CreateTests < ActiveRecord::Migration[5.1]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
+      t.string :title, null: false
+      t.integer :level, null: false, default: 0
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210224142855_create_questions.rb
+++ db/migrate/20210224142855_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
-      t.string :body
+      t.string :title, null: false
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
